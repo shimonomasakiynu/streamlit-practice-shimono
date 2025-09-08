724a0772bc7ac3943a3fb15324fa8b50e269eafc
shimonomasakiynu <shimono-masaki-rb@ynu.jp>
2025-09-08 11:50:10 +0900

    追加

--- app.py
+++ app.py
@@ -2,3 +2,10 @@ import streamlit as st
 st.title("Hello, Streamlit!")
 st.write("これは最小構成の Streamlitアプリです。")
 st.write("Shimono0908")
+#数値を入力するウィジェット
+number = st.number_input("数値を入力してください", min_value=0, max_value=100, value=50, step=1)
+st.write(f"入力された数値: {number}")
+#ボタンを追加
+if st.button("クリックしてね"):
+    st.write("ボタンがクリックされました！")
+    
