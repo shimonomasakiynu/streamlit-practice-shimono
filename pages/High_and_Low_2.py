494ab43b6ec67ee4f1950f828e770a4afb265bca
shimonomasakiynu <shimono-masaki-rb@ynu.jp>
2025-09-22 12:07:22 +0900

    あ

--- pages/High_and_Low_2.py
+++
@@ -1,7 +0,0 @@
-import json
-from pathlib import Path
-
-# ----Pathを指定して JSONファイルを読み込み ----
-json_path = Path(__file__).parent.parent / "sample_data" / "highlow_round3.json"
-with open(json_path, "r", encoding="utf-8") as f:
-    data = json.load(f)
